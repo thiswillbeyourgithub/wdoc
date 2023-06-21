8d19859749b7411ba29be381a52a8f0e56f2cd08
thiswillbeyourgithub <github@32mail.33mail.com>
2023-06-21 19:17:07 +0200

    added new pdf parsers

--- utils/file_loader.py
+++ utils/file_loader.py
@@ -17,6 +17,8 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document
 from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import PDFMinerPDFasHTMLLoader
+from langchain.document_loaders import PDFMinerLoader
 from langchain.document_loaders import YoutubeLoader
 from langchain.vectorstores import FAISS
 
@@ -155,9 +157,72 @@ def load_doc(filetype, debug, **kwargs):
         path = kwargs["path"]
         whi(f"Loading pdf: '{path}'")
         assert Path(path).exists(), f"file not found: '{path}'"
-        loader = PyPDFLoader(path)
+
+        #loader = PyPDFLoader(path)
+        loader = PDFMinerLoader(path)
         docs = loader.load()
         docs = loaddoc_cache.eval(text_splitter.transform_documents, docs)
+
+        # source: https://python.langchain.com/docs/modules/data_connection/document_loaders/how_to/pdf
+        # loader = PDFMinerPDFasHTMLLoader(path)
+        # data = loader.load()[0]   # entire pdf is loaded as a single Document
+        # def parsePDFasHTML(data):
+        #     soup = BeautifulSoup(data.page_content,'html.parser')
+        #     content = soup.find_all('div')
+        #     cur_fs = None
+        #     cur_text = ''
+        #     snippets = []   # first collect all snippets that have the same font size
+        #     for c in content:
+        #         sp = c.find('span')
+        #         if not sp:
+        #             continue
+        #         st = sp.get('style')
+        #         if not st:
+        #             continue
+        #         fs = re.findall('font-size:(\d+)px',st)
+        #         if not fs:
+        #             continue
+        #         fs = int(fs[0])
+        #         if not cur_fs:
+        #             cur_fs = fs
+        #         if fs == cur_fs:
+        #             cur_text += c.text
+        #         else:
+        #             snippets.append((cur_text,cur_fs))
+        #             cur_fs = fs
+        #             cur_text = c.text
+        #     snippets.append((cur_text,cur_fs))
+        #     # Note: The above logic is very straightforward. One can also add more strategies such as removing duplicate snippets (as
+        #     # headers/footers in a PDF appear on multiple pages so if we find duplicatess safe to assume that it is redundant info)
+        #     cur_idx = -1
+        #     semantic_snippets = []
+        #     # Assumption: headings have higher font size than their respective content
+        #     for s in snippets:
+        #         # if current snippet's font size > previous section's heading => it is a new heading
+        #         if not semantic_snippets or s[1] > semantic_snippets[cur_idx].metadata['heading_font']:
+        #             metadata={'heading':s[0], 'content_font': 0, 'heading_font': s[1]}
+        #             metadata.update(data.metadata)
+        #             semantic_snippets.append(Document(page_content='',metadata=metadata))
+        #             cur_idx += 1
+        #             continue
+        #         
+        #         # if current snippet's font size <= previous section's content => content belongs to the same section (one can also create
+        #         # a tree like structure for sub sections if needed but that may require some more thinking and may be data specific)
+        #         if not semantic_snippets[cur_idx].metadata['content_font'] or s[1] <= semantic_snippets[cur_idx].metadata['content_font']:
+        #             semantic_snippets[cur_idx].page_content += s[0]
+        #             semantic_snippets[cur_idx].metadata['content_font'] = max(s[1], semantic_snippets[cur_idx].metadata['content_font'])
+        #             continue
+        #         
+        #         # if current snippet's font size > previous section's content but less tha previous section's heading than also make a new 
+        #         # section (e.g. title of a pdf will have the highest font size but we don't want it to subsume all sections)
+        #         metadata={'heading':s[0], 'content_font': 0, 'heading_font': s[1]}
+        #         metadata.update(data.metadata)
+        #         semantic_snippets.append(Document(page_content='', metadata=metadata))
+        #         cur_idx += 1
+        #     docs = text_splitter.transform_documents(semantic_snippets)
+        #     return docs
+        # docs = loaddoc_cache.eval(parsePDFasHTML, data)
+
 
     elif filetype == "anki":
         for nk in ["anki_deck", "anki_notetype", "anki_profile", "anki_fields"]:
