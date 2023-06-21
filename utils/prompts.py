85e59cfe89c74257f31812fa614b59e9fd0f266d
thiswillbeyourgithub <github@32mail.33mail.com>
2023-06-21 14:51:29 +0200

    better prompt

--- utils/prompts.py
+++ utils/prompts.py
@@ -1,38 +1,45 @@
 from langchain import PromptTemplate, LLMChain
 
-prompt_template = """Your job is to write a summary of the beginning of a text.
-You have to format each information as markdown bullet points.
-You have to use indentation to organize those bullet points hierarchically.
-You have to write in the same language as the input text.
-You have to ignore information not relevant to the topic of the text (sponsors, advertissement, headers, urls, etc)
+prompt_template = """Your job is to write a summary of a text.
 
+Here are the rules:
+All relevant information, anecdotes, facts, etc must appear in your summary.
+You have to use markdown bulletpoints to format your summary.
+The bulletpoints can be indented to help organize information hierarchically.
+Your summary will be written in the same language as the input text.
+Irrelevant information are to be discarded, for example sponsors, advertissement, embellishment, headers etc
+
+Here's the first part of the text:
 '''
 {text}
 '''
 
-SUMMARY OF INFORMATIONS IN MARKDOWN AND IN THE SAME LANGUAGE:
+EXECUTIVE SUMMARY IN MARKDOWN AND WITHOUT TRANSLATION:
 """
 PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
 refine_template = (
-    """You received the summary of the beginning of a text and your job is to continue the summary based on the next part of the text.
-You have to format each information as markdown bullet points.
-You have to use indentation to organize those bullet points hierarchically.
-You have to write in the same language as the input text.
-You have to ignore information not relevant to the topic of the text (sponsors, advertissement, headers, urls, etc)
+    """Your job is to continue the summary of a text.
 
-Here's the summary of the text so far:
+Here are the rules:
+All relevant information, anecdotes, facts, etc must appear in your summary.
+You have to use markdown bulletpoints to format your summary.
+The bulletpoints can be indented to help organize information hierarchically.
+Your summary will be written in the same language as the input text.
+Irrelevant information are to be discarded, for example sponsors, advertissement, embellishment, headers etc
+
+Here's the summary so far:
 '''
 {existing_answer}
 '''
 
-Here's the next part of the text:
+Here's the next section of the text:
 '''
 {text}
 '''
 
-Given this new part of the document, refine the summary as logically indented markdown bullet points. If the new part is not worth it, simply return the original summary.
+Given this new section of the text, refine the summary. If no changes are needed, simply answer the original summary.
 
-SUMMARY OF NEW INFORMATIONS IN MARKDOWN AND IN THE SAME LANGUAGE:
+EXECUTIVE SUMMARY IN MARKDOWN AND WITHOUT TRANSLATION:
 """
 )
 refine_prompt = PromptTemplate(
