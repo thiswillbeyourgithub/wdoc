193aa92ee96f03a62cbea641cb3150a353e84551
thiswillbeyourgithub <github@32mail.33mail.com>
2023-06-24 13:24:01 +0200

    fix prompt

--- utils/prompts.py
+++ utils/prompts.py
@@ -2,20 +2,20 @@ from langchain import PromptTemplate, LLMChain
 summary_rules = """
 Here are the rules:
     - Regarding the formatting of your summary:
-        - You have to use markdown bulletpoints to format your summary.
-        - The bulletpoints can be indented to help organize information hierarchically.
+        - You have to use markdown bullet points to format your summary.
+        - The bullet points can be indented to help organize information hierarchically.
         - These rules are an example of proper formatting.
     - Regarding the content of your summary:
-        - All relevant information, anecdotes, facts, etc must appear in your summary.
-        - Your summary has to be written in the same language as the text: if the text is in french, write a summary in french.
-        - Information has to appear in roughly the same order as in the text. You can deviate a bit from the original ordering if it helps the hierarchization and helps to compress the summary.
-        - Irrelevant information are to be discarded, for example sponsors, advertissement, embellishment, headers etc
-        - The summary has to be brief, to the point, compressed.
-            - A fine example of brievty is the daily staff memo to the US President.
+        - All relevant information, anecdotes, facts, etc. must appear in your summary.
+        - Your summary has to be written in the same language as the text: if the text is in French, write a summary in French.
+        - Information has to appear in roughly the same order as in the text. You can deviate a bit from the original ordering if it helps with the hierarchization and helps to compress the summary.
+        - Irrelevant information is to be discarded, for example sponsors, advertisements, embellishments, headers, etc.
+        - The summary has to be brief, to the point, and compressed.
+            - A fine example of brevity is the daily staff memo to the US President.
             - To be quick to read, use as much as possible common words.
-        - If relevant, you can use direct quotation form the text.
+        - If relevant, you can use direct quotations from the text.
         - You absolutely have to be truthful and unbiased.
-    - Additionally, if the text comes with a title, you must explicitely write a sentence that answers directly to the title. i.e. if the title is a question, answer it. if the title is clickbaity, write a satisfactory explanation etc.
+    - Additionally, if the text comes with a title, you must explicitly write a sentence that answers directly to the title. i.e. if the title is a question, answer it. If the title is clickbaity, write a satisfactory explanation, etc.
         - In which case, this bullet point must begin with "TITLE EXPLAINER:" and appear as the first bullet point.
 """
 
