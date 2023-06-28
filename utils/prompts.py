0be49200876842fd819eab70ebeb5fb7dda79bd7
thiswillbeyourgithub <github@32mail.33mail.com>
2023-06-28 14:51:55 +0200

    fix wrong indentation for prompt

--- utils/prompts.py
+++ utils/prompts.py
@@ -3,21 +3,21 @@ from textwrap import dedent
 # rule for the summarization
 summary_rules = dedent("""
 * Regarding the formatting of your summary:
-    * Use markdown bullet points.
-    * Use indentation to organize information hierarchically.
-    * The bullet points for each information have to follow the order of appearance in the text.
-    * The present rules are a good example of adequate formatting.
+  * Use markdown bullet points.
+  * Use indentation to organize information hierarchically.
+  * The bullet points for each information have to follow the order of appearance in the text.
+  * The present rules are a good example of adequate formatting.
 * Regarding the content of your summary:
-    * Include all noteworthy information, anecdotes, facts, insights, definitions, clarifications, explanations, ideas, etc.
-    * Exclude sponsors, advertisements, embellishments, headers, etc.
-    * When in doubt about wether to include an information or not, include it.
-    * Direct quotations are allowed.
-    * Your answer can contain many bullet points but each one has to be brief and to the point. You don't have to use complete sentences.
-        * Good example of brevity : US President's daily staff memo, the present rules.
-        * Use common words but keep technical details if noteworthy.
-        * Don't use pronouns if it's implied by the previous bullet point: write like a technical report.
-    * Write in the same language as the input: if the text is in French, write an answer in French.
-    * Write without bias and stay faithful to the author.
+  * Include all noteworthy information, anecdotes, facts, insights, definitions, clarifications, explanations, ideas, etc.
+  * Exclude sponsors, advertisements, embellishments, headers, etc.
+  * When in doubt about wether to include an information or not, include it.
+  * Direct quotations are allowed.
+  * Your answer can contain many bullet points but each one has to be brief and to the point. You don't have to use complete sentences.
+    * Good example of brevity : US President's daily staff memo, the present rules.
+    * Use common words but keep technical details if noteworthy.
+    * Don't use pronouns if it's implied by the previous bullet point: write like a technical report.
+  * Write in the same language as the input: if the text is in French, write an answer in French.
+  * Write without bias and stay faithful to the author.
 """.strip())
 
 # template to summarize
