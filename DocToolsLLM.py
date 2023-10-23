4ce4542fa1ff7296ee5c7a4f5ec301516e14597f
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-10-23 13:50:51 +0200

    increase logit bias of indentation

--- DocToolsLLM.py
+++ DocToolsLLM.py
@@ -370,12 +370,12 @@ class DocToolsLLM:
                 # increase likelyhood that chatgpt will use indentation by
                 # biasing towards adding space.
                 self.llm.model_kwargs["logit_bias"] = {
-                        12: 2,  # '-'
+                        12: 3,  # '-'
                         # 220: 1,  # ' '
                         # 532: 1,  # ' -'
                         #9: 10,  # '*'
                         #1635: 10,  # ' *'
-                        197: 2,  # '\t'
+                        197: 3,  # '\t'
                         }
                 self.llm.model_kwargs["frequency_penalty"] = 0.5
                 self.llm.model_kwargs["temperature"] = 0.2
