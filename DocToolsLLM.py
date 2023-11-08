7eb69c1b104610ba0546677ea55049fe7c902ae7
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-11-08 10:25:06 +0100

    bump version

--- DocToolsLLM.py
+++ DocToolsLLM.py
@@ -34,7 +34,7 @@ d = datetime.today()
 today = f"{d.day:02d}/{d.month:02d}/{d.year:04d}"
 
 class DocToolsLLM:
-    VERSION = 0.8
+    VERSION = 0.9
     def __init__(
             self,
             model="openai",
