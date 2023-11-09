b61c006f4dc380bbb7e9ee56dbe80d5ad8966f19
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-11-09 13:31:10 +0100

    insist on continuing indentation when chunksd

--- utils/tasks.py
+++ utils/tasks.py
@@ -84,7 +84,7 @@ def do_summarize(
 
             summaries.append(out["output_text"].rstrip())
 
-            previous_summary = f"End of the summary of the previous section (avoid overlap by directly starting a new bullet point):\n'''\n{summaries[-1]}\n'''"
+            previous_summary = f"End of the summary of the previous section (avoid overlap by directly starting a new bullet point but don't forget to continue with the same indentation if needed):\n'''\n{summaries[-1]}\n'''"
             if metadata:
                 previous_summary = "\n\n" + previous_summary
 
