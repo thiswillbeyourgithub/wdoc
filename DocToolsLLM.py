7b800dbdf0fdbb680dd7bc4484494d8eb06ecc3d
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-11-09 10:03:24 +0100

    count the number of removed documentd

--- DocToolsLLM.py
+++ DocToolsLLM.py
@@ -223,6 +223,7 @@ class DocToolsLLM:
                         "Same metadata object is used to store information on "
                         "multiple documents!")
                 hashes = [d.metadata["hash"] for d in self.loaded_docs]
+                nremoved = 0
                 if len(set(hashes)) != len(hashes):
                     red("Found duplicate hashes after loading documents:")
                     for i, doc in enumerate(self.loaded_docs):
@@ -231,7 +232,10 @@ class DocToolsLLM:
                             red(f"  * Removed #{i}: {doc}")
                             self.loaded_docs[i] = None
                             n -= 1
+                            nremoved += 1
                     self.loaded_docs = [d for d in self.loaded_docs if d is not None]
+                    red(f"Removed {nremoved}/{len(hashes)} documents because they had the same hash")
+
 
         else:
             self.loaded_docs = None  # will be loaded when embeddings are loaded
