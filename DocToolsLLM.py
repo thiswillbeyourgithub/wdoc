27fbe40bb79e461df4f8342bb1131462c0e68f59
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-12-10 11:16:45 +0100

    set recursive summary to 1 by default

--- DocToolsLLM.py
+++ DocToolsLLM.py
@@ -53,7 +53,7 @@ class DocToolsLLM:
             loadfrom=None,
 
             top_k=10,
-            n_recursive_summary=0,
+            n_recursive_summary=1,
             n_summaries_target=-1,
 
             debug=False,
@@ -125,8 +125,9 @@ class DocToolsLLM:
         --top_k int, default 10
             number of chunks to look for when querying
 
-        --n_recursive_summary int, default 0
-            will always recursively summarize
+        --n_recursive_summary int, default 1
+            will recursively summarize the summary this many times.
+            1 means that the original summary will be summarize. 0 means disabled.
 
         --n_summaries_target int, default -1
             Only active if query is 'summarize_link_file'. Set a limit to
