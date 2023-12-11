83d1984d5904d19e2c4e70fe605d2c13ad32549a
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-12-11 17:22:45 +0100

    fix: search task

--- utils/file_loader.py
+++ utils/file_loader.py
@@ -97,7 +97,7 @@ def get_tkn_length(tosplit):
 
 def get_splitter(task):
     "we don't use the same text splitter depending on the task"
-    if task == "query":
+    if task in ["query", "search"]:
         text_splitter = RecursiveCharacterTextSplitter(
                 separators=["\n\n\n\n", "\n\n\n", "\n\n", "\n", " ", ""],
                 chunk_size=3000,  # default 4000
