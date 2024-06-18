b4f7a66d2e88dc8850adb4c6686619cf459259c9
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-06-18 13:20:01 +0200

    fix: modelname for openai

--- DocToolsLLM/utils/llm.py
+++ DocToolsLLM/utils/llm.py
@@ -86,7 +86,7 @@ def load_llm(
         if max_tokens not in extra_model_args:
             extra_model_args["max_tokens"] = max_tokens
         llm = ChatOpenAI(
-                model_name=modelname,
+                model_name=modelname.split("/")[1],
                 cache=not no_llm_cache,
                 verbose=verbose,
                 callbacks=[PriceCountingCallback(verbose=verbose)],
