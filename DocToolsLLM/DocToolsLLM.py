53ffc48882f91a5169e4cf2da48f82e91a276bd1
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-06-21 16:16:26 +0200

    bump version 0.39 -> 0.40

--- DocToolsLLM/DocToolsLLM.py
+++ DocToolsLLM/DocToolsLLM.py
@@ -78,7 +78,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "true"
 class DocToolsLLM_class:
     "This docstring is dynamically replaced by the content of DocToolsLLM/docs/USAGE.md"
 
-    VERSION: str = "0.39"
+    VERSION: str = "0.40"
 
     #@optional_typecheck
     @typechecked
