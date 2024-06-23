303e2fe2995c0a9ee73a721d45c6a5df75e7b051
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-06-23 09:56:13 +0200

    better short description

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open("README.md", "r") as readme:
 setup(
     name="DocToolsLLM",
     version="0.40",
-    description="A perfect RAG and AI summary setup for my needs. Supports all LLM, virt. any filetypes (epub, youtube_playlist, pdf, mp3, etc)",
+    description="A perfect AI powered RAG for document query and summary. Supports ~all LLM and ~all filetypes (url, pdf, epub, youtube (incl playlist), audio, anki, md, docx, pptx, oe any combination!)",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/thiswillbeyourgithub/DocToolsLLM/",
