32c1220dead0f76a6e2ec7625f1d2b505cde1314
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-06-23 12:59:25 +0200

    fix: linux platform detector

--- DocToolsLLM/utils/flags.py
+++ DocToolsLLM/utils/flags.py
@@ -7,11 +7,10 @@ import platform
 
 # parse args again to know globally if we're in verbose mode
 kwargs = fire.Fire(lambda *args, **kwargs: kwargs)
+is_linux = platform.system() == "Linux"
 if "debug" in kwargs and kwargs["debug"]:
     is_debug = True
     is_verbose = True
-    is_linux = platform.system() == "Linux"
 else:
     is_debug = False
     is_verbose = False
-    is_linux = False
