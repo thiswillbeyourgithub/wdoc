7097b3e137a0771017dcf4114101506f19926fad
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-07-04 18:26:09 +0200

    fix: can't use typechecker like that in joblib parallel

--- DocToolsLLM/utils/batch_file_loader.py
+++ DocToolsLLM/utils/batch_file_loader.py
@@ -241,7 +241,6 @@ def batch_load_doc(
                     to_load[idoc]["load_functions"] = parse_load_functions(tuple(doc["load_functions"]))
 
     # wrap doc_loader to cach errors cleanly
-    @optional_typecheck
     def load_one_doc_wrapped(**doc_kwargs) -> Union[List[Document], str]:
         try:
             out = load_one_doc(**doc_kwargs)
