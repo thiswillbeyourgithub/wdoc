4aad00aa490c8d20c881471ebc36cbc0bc0d4ded
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-07-21 21:08:28 +0200

    bump version 1.1.9 -> 1.1.10

--- WDoc/WDoc.py
+++ WDoc/WDoc.py
@@ -79,7 +79,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "true"
 class WDoc:
     "This docstring is dynamically replaced by the content of WDoc/docs/USAGE.md"
 
-    VERSION: str = "1.1.9"
+    VERSION: str = "1.1.10"
     allowed_extra_args = extra_args_keys
     md_printer = md_printer
 
