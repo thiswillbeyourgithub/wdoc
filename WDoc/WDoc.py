490c7d8d6407cd76668400fd72c0c7c8b5e83f2c
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-07-29 18:46:58 +0200

    bump version 1.2.1 -> 1.2.2

--- WDoc/WDoc.py
+++ WDoc/WDoc.py
@@ -83,7 +83,7 @@ else:
 class WDoc:
     "This docstring is dynamically appended the content of WDoc/docs/USAGE.md"
 
-    VERSION: str = "1.2.1"
+    VERSION: str = "1.2.2"
     allowed_extra_args = extra_args_keys
     md_printer = md_printer
 
