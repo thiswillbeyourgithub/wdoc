400d693c6cc7067b0589fb4c14e73f3eb70e1dc0
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-08-03 20:12:38 +0200

    fix: limit number of decimal of price in the fiche

--- examples/TheFiche/TheFiche.py
+++ examples/TheFiche/TheFiche.py
@@ -66,7 +66,7 @@ def run_wdoc(query: str, kwargs2: dict) -> Tuple[WDoc, dict]:
         "WDoc_model": f"{instance.modelname} of {instance.modelbackend}",
         "WDoc_evalmodel": f"{instance.query_eval_modelname} of {instance.query_eval_modelbackend}",
         "WDoc_evalmodel_check_nb": instance.query_eval_check_number,
-        "WDoc_cost": instance.latest_cost,
+        "WDoc_cost": f"{float(instance.latest_cost):.5f}",
         "WDoc_n_docs_found": len(fiche["unfiltered_docs"]),
         "WDoc_n_docs_filtered": len(fiche["filtered_docs"]),
         "WDoc_n_docs_used": len(fiche["relevant_filtered_docs"]),
