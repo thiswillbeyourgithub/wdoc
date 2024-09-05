09284cc0340835cb201d6fc21f765153251b562b
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-09-05 18:01:11 +0200

    add typechecking to env variables

--- WDoc/utils/env.py
+++ WDoc/utils/env.py
@@ -13,10 +13,10 @@ def parse(val: str) -> Any:
     return val
 
 WDOC_TYPECHECKING = "warn"
-WDOC_NO_MODELNAME_MATCHING = None
-WDOC_ALLOW_NO_PRICE = None
+WDOC_NO_MODELNAME_MATCHING = False
+WDOC_ALLOW_NO_PRICE = False
 WDOC_OPEN_ANKI = False
-WDOC_STRICT_DOCDICT = None
+WDOC_STRICT_DOCDICT = False
 WDOC_MAX_LOADER_TIMEOUT = 30 * 60
 WDOC_MAX_PDF_LOADER_TIMEOUT = 5 * 60
 WDOC_PRIVATE_MODE = False
@@ -25,9 +25,25 @@ WDOC_EXPIRE_CACHE_DAYS = 0
 WDOC_EMPTY_LOADER = False
 WDOC_BEHAVIOR_EXCL_INCL_USELESS = "warn"
 
+valid_types = {
+    'WDOC_TYPECHECKING': str,
+    'WDOC_NO_MODELNAME_MATCHING': bool,
+    'WDOC_ALLOW_NO_PRICE': bool,
+    'WDOC_OPEN_ANKI': bool,
+    'WDOC_STRICT_DOCDICT': bool,
+    'WDOC_MAX_LOADER_TIMEOUT': int,
+    'WDOC_MAX_PDF_LOADER_TIMEOUT': int,
+    'WDOC_PRIVATE_MODE': bool,
+    'WDOC_DEBUGGER': bool,
+    'WDOC_EXPIRE_CACHE_DAYS': int,
+    'WDOC_EMPTY_LOADER': bool,
+    'WDOC_BEHAVIOR_EXCL_INCL_USELESS': str,
+}
+
 for k in os.environ.keys():
     if not k.startswith("WDOC_"):
         continue
     v = parse(os.environ[k])
     assert k in locals().keys(), f"Unexpected key for WDOC env variable: {k}"
+    assert isinstance(v, valid_types[k]), f"Unexpected type of env variable '{k}': '{type(v)}' but expected '{valid_types['k']}'"
     locals()[k] = v
