a1fa1991fef0283540aaacb8064666a6dbd4fe1a
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-09-25 09:36:51 +0200

    fix: add missing help from included files

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/thiswillbeyourgithub/WDoc/",
     packages=find_packages(),
+    package_data={"wdoc": ["WDoc/docs/USAGE.md"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
