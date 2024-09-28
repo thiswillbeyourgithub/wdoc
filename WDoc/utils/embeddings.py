6571186c1b348a279993eadadee7f9c443f73da7
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-09-28 19:52:13 +0200

    typo

--- WDoc/utils/embeddings.py
+++ WDoc/utils/embeddings.py
@@ -332,7 +332,7 @@ def load_embeddings(
     whi("Waiting for answers")
     merged_dbs = []
     for iq, q in enumerate(loader_queues):
-        assert loader_workers[iq].is_alive(), f"Loader worker #{ind} is dead"
+        assert loader_workers[iq].is_alive(), f"Loader worker #{iq} is dead"
         while True:
             try:
                 whi(f"Waiting for partial db from loader worker #{iq}")
@@ -563,7 +563,7 @@ def load_embeddings(
             red(f"Some faiss saver workers failed to stop: {len([t for t in saver_workers if t.is_alive()])}/{len(saver_workers)}")
         out_vals = []
         for iq, q in enumerate(saver_queues):
-            assert saver_queues[iq].is_alive(), f"Saver worker #{ind} is dead"
+            assert saver_queues[iq].is_alive(), f"Saver worker #{iq} is dead"
             while True:
                 try:
                     whi(f"Waiting for confirmation from saver worker #{iq}")
