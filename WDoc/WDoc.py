2fe5a228dc38d0b4f6f5d41804f6b36babf36b71
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-09-30 20:24:37 +0200

    bump version 2.1.2 -> 2.1.3

--- WDoc/WDoc.py
+++ WDoc/WDoc.py
@@ -79,7 +79,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "true"
 class WDoc:
     "This docstring is dynamically appended the content of WDoc/docs/USAGE.md"
 
-    VERSION: str = "2.1.2"
+    VERSION: str = "2.1.3"
     allowed_extra_args = extra_args_types
     md_printer = md_printer
 
