81366069516c031a1b9f2cc61b07c9c56d30911a
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-10-04 15:49:43 +0200

    potentially better __main__

--- WDoc/__main__.py
+++ WDoc/__main__.py
@@ -3,7 +3,7 @@ Entry point used when WDoc is imported or called by 'python -m WDoc'.
 Does the same as __init__.py
 """
 
-from . import cli_launcher
 
 if __name__ == "__main__":
+    from . import cli_launcher
     cli_launcher()
