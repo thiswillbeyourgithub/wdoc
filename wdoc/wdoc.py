7d6570b1870b7a35231d46a685d31a6ea282747a
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-02-09 18:28:23 +0100

    bump version 2.5.4 -> 2.5.5

--- wdoc/wdoc.py
+++ wdoc/wdoc.py
@@ -110,7 +110,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "true"
 class wdoc:
     "This docstring is dynamically appended the content of wdoc/docs/USAGE.md"
 
-    VERSION: str = "2.5.3"
+    VERSION: str = "2.5.5"
     allowed_extra_args = extra_args_types
     md_printer = md_printer
 
