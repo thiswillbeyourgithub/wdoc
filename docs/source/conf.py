18bc52128fa381ceac8596e78ba6bdef5b84abc6
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-02-11 17:14:40 +0100

    bump version 2.5.5 -> 2.5.6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ def setup(app):
 project = "wdoc"
 copyright = "2024, thiswillbeyourgithub"
 author = "thiswillbeyourgithub"
-release = "2.5.5"
+release = "2.5.6"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
