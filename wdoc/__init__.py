5bbeaead9dedd79e27d49fc441df3f707a86cb6e
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-02-28 16:24:10 +0100

    fix: add a try block around patching asyncio loop

--- wdoc/__init__.py
+++ wdoc/__init__.py
@@ -1,8 +1,11 @@
 # needed to fix ollama 'event loop closed' error
 # thanks to https://github.com/BerriAI/litellm/pull/7625/files
-import nest_asyncio
+try:
+    import nest_asyncio
 
-nest_asyncio.apply()
+    nest_asyncio.apply()
+except Exception as e:
+    print(f"Failed to patch asyncio loop using nest_asyncio. Error: '{e}'")
 
 from . import utils
 from .wdoc import wdoc
