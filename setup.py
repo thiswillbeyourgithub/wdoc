6d01207313bbe0ccfc84bb4d23735da6b83c8ada
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-03-03 20:16:10 +0100

    bump version 2.6.5 -> 2.6.6

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="wdoc",
-    version="2.6.5",
+    version="2.6.6",
     description="A perfect AI powered RAG for document query and summary. Supports ~all LLM and ~all filetypes (url, pdf, epub, youtube (incl playlist), audio, anki, md, docx, pptx, oe any combination!)",
     long_description=long_description,
     long_description_content_type="text/markdown",
