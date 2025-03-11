ce839df6c497d7a8a5023fb30da782bdcef6d321
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-03-11 21:42:12 +0100

    bump version 2.7.0 -> 2.7.1

--- wdoc/wdoc.py
+++ wdoc/wdoc.py
@@ -124,7 +124,7 @@ class wdoc:
     This docstring is dynamically updated with the content of wdoc/docs/help.md
     """
 
-    VERSION: str = "2.7.0"
+    VERSION: str = "2.7.1"
     allowed_extra_args = extra_args_types
     md_printer = md_printer
 
