dfccef092e15c171833909e40c4cf15ddc2fbddc
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-03-13 19:17:10 +0100

    minor
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- wdoc/__init__.py
+++ wdoc/__init__.py
@@ -1,3 +1,11 @@
+import os
+
+# sensible default user agent
+os.environ["USER_AGENT"] = os.environ.get(
+    "USER_AGENT",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
+)
+
 # needed to fix ollama 'event loop closed' error
 # thanks to https://github.com/BerriAI/litellm/pull/7625/files
 try:
@@ -6,14 +14,6 @@ try:
     nest_asyncio.apply()
 except Exception as e:
     print(f"Failed to patch asyncio loop using nest_asyncio. Error: '{e}'")
-
-import os
-
-# sensible default user agent
-os.environ["USER_AGENT"] = os.environ.get(
-    "USER_AGENT",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
-)
 
 from . import utils
 from .wdoc import wdoc
