7087501f526991adb03646f69f5e265895e6fade
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-04-16 14:52:11 +0200

    test: better test by checking if the default install works too
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -7,14 +7,18 @@ uv venv test_venv &&\
 source test_venv/bin/activate &&\
 sleep 1 &&\
 uv pip install -e ".." &&\
-uv pip install -e "..[fasttext]" &&\
-uv pip install -e "..[pdftotextt]" &&\
 uv pip install pytest pytest-xdist &&\
 sleep 1 &&\
 mkdir temp &&\
 cd temp &&\
 python -m pytest ../test_wdoc.py -n auto -v && \
 python -m pytest ../test_wdoc.py -m api -v &&\
+# also check if we can install those then redo some of the tests
+uv pip install -e "..[fasttext]" &&\
+uv pip install -e "..[pdftotextt]" &&\
+python -m pytest ../test_wdoc.py -n auto -v && \
+# check if we can install the dev test
+uv pip install -e "..[dev]" &&\
 cd .. &&\
 deactivate &&\
 trash test_venv temp &&\
