802da74c2e212981a45f1b76cbfe4949a04cc595
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-04-16 18:30:15 +0200

    better test script
    Signed-off-by: thiswillbeyourgithub
    <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,25 +1,46 @@
 #!/bin/zsh
 
+set -e
+set -o pipefail
+set -u
+
+# cleanup previous
+[[ "$(type deactivate)" == "deactivate is a shell function"* ]] && deactivate
+[ -e "temp" ] && trash temp
+[ -e "test_env" ] && trash test_env
+[ -e "__pycache__" ] && trash __pycache__
+
+# setup venv
+uv venv test_venv
+source test_venv/bin/activate
+sleep 1
+
+# install wdoc
+uv pip install -e ".."
+
+# install test suite
+uv pip install pytest pytest-xdist
+sleep 1
+mkdir temp
+
+# start tests
+cd temp
+python -m pytest ../test_wdoc.py -n auto -v && \
+python -m pytest ../test_wdoc.py -m api -v
+
+# also check if we can install those then redo some of the tests
+cd ..
+uv pip install -e "..[fasttext]"
+uv pip install -e "..[pdftotextt]"
+cd temp
+python -m pytest ../test_wdoc.py -n auto -v && \
+
+# check if we can install the dev test
+cd ..
+uv pip install -e "..[dev]"
+
+# cleanup
 deactivate
 trash test_venv temp
-
-uv venv test_venv &&\
-source test_venv/bin/activate &&\
-sleep 1 &&\
-uv pip install -e ".." &&\
-uv pip install pytest pytest-xdist &&\
-sleep 1 &&\
-mkdir temp &&\
-cd temp &&\
-python -m pytest ../test_wdoc.py -n auto -v && \
-python -m pytest ../test_wdoc.py -m api -v &&\
-# also check if we can install those then redo some of the tests
-uv pip install -e "..[fasttext]" &&\
-uv pip install -e "..[pdftotextt]" &&\
-python -m pytest ../test_wdoc.py -n auto -v && \
-# check if we can install the dev test
-uv pip install -e "..[dev]" &&\
-cd .. &&\
-deactivate &&\
-trash test_venv temp &&\
+cd ..
 echo "Succesfuly ran all tests"
