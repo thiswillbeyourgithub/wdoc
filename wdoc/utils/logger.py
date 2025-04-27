415b03c0a7cc3817cdae83ec8721b99de03adbb2
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-04-27 17:00:10 +0200

    set an info print to debug
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- wdoc/utils/logger.py
+++ wdoc/utils/logger.py
@@ -74,7 +74,7 @@ logger.add(
     diagnose=True if env.WDOC_DEBUG else None,
 )
 
-logger.info(f"log_file location: {log_file}")
+logger.debug(f"log_file location: {log_file}")
 
 console = Console()
 
