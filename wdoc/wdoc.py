03aeab2db4daae721f06b67d832492df423bdc31
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-05-03 19:03:48 +0200

    bump version 3.2.0 -> 3.2.1

--- wdoc/wdoc.py
+++ wdoc/wdoc.py
@@ -103,7 +103,7 @@ class wdoc:
     This docstring is dynamically updated with the content of wdoc/docs/help.md
     """
 
-    VERSION: str = "3.2.0"
+    VERSION: str = "3.2.1"
     allowed_extra_args = extra_args_types
     __import_mode__: bool = True
 
