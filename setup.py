cfff0aca6504eaa0e7c04608fc9ba256911522fd
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-05-17 20:14:08 +0200

    fix: bump version of litellm because windows crashes
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -142,7 +142,7 @@ setup(
         "langchain-community>=0.3.1",
         "langchain-openai>=0.2.1",
         "langfuse>=2.59.3",  # for observability
-        "litellm==v1.67.6",
+        "litellm==v1.69.3",
         "nest_asyncio>=1.6.0",  # needed to fix ollama 'event loop closed' error thanks to https://github.com/BerriAI/litellm/pull/7625/files
         "prompt-toolkit>=3.0.47",
         "tqdm>=4.66.4",
